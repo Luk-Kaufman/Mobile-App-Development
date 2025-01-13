918d8bcca95f1bfcdcbccd02b9db00afebdb5778
Luke Kaufman <kaufman.53@wright.edu>
2025-01-12 20:08:00 -0500

    Code for Mobile Game

---
+++ Bowling_App/settings.gradle.kts
@@ -0,0 +1,20 @@
+enableFeaturePreview("VERSION_CATALOGS")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Bowling_App"
+include(":app")
